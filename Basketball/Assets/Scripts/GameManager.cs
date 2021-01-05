20edcadbb0c7c3982e4b87d1a670b9d5302feda7
Kuo0418 <71679871+Kuo0418@users.noreply.github.com>
2021-01-05 12:58:29 +0800

    勝推送

---
+++ Basketball/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("籃球場");
+    }
+
+    public void QuitGame()
+    {
+        //應用程式.離開遊戲
+        Application.Quit();
+    }
+}
